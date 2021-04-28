dca359e16a1247ecc2bd4ba0aa6bf29298e5a602
Carlos Martinez <curlosmart@gmail.com>
2021-04-28 17:35:42 -0400

    table ordered in descending sequence

--- script.js
+++ script.js
@@ -6,34 +6,44 @@ let words = [
     {count: 520, word: 'and'}
 ]
 
-let wordCounts = new Map()
+let wordCountsMap = new Map()
 
 document.getElementById('count-words').addEventListener('click', () => {
     let wordStr = document.getElementById('words-input').value.split(' ')
-    console.log(wordStr)
 
     wordStr.map((word) => {
         let newWord = word.replace('\n', '').toLowerCase()
-        wordCount = wordCounts[newWord]
+        wordCount = wordCountsMap[newWord]
 
-        wordCounts[newWord] = wordCount ? wordCount + 1 : 1;
+        wordCountsMap[newWord] = wordCount ? wordCount + 1 : 1;
     })
 
-    sortedWordCounts = new Map([...wordCounts].sort((a, b) => wordCounts[a]))
+    let wordCountArr = [];
 
-    console.log(wordCounts);
+    for (var key in wordCountsMap) {
+        wordCountArr.push({
+        word: key,
+        count: wordCountsMap[key]
+    });
+    }
+
+    var sortedWordCount = wordCountArr.sort(function(a, b) {
+        return (b.count > a.count) ? 1 : ((a.count > b.count) ? -1 : 0)
+    });
+
+    console.log(sortedWordCount)
 
     let table = document.querySelector("table");
-    generateTable(table, wordCounts);
+    generateTable(table, sortedWordCount);
 })
 
 
 
 const generateTable = (table, wordCounts) => {
     rankNum = 1
-    Object.keys(wordCounts).map((word) => {
+    wordCounts.map((wordInfo) => {
         let row = table.insertRow();
-        let rowElems = [rankNum, word, wordCounts[word]]
+        let rowElems = [rankNum, wordInfo.count, wordInfo.word]
 
         rowElems.map((elem) => {
             let cell = row.insertCell();
@@ -43,22 +53,4 @@ const generateTable = (table, wordCounts) => {
 
         rankNum++;
     })
-
-    // for (let key of data) {
-    //     console.log(element)
-    //     let row = table.insertRow();
-
-    //     console.log()
-
-    //     let rankCell = row.insertCell();
-    //     let rank = document.createTextNode(rankNum);
-    //     rankCell.appendChild(rank);
-    //     rankNum++;
-
-    //     // for (key in element) {
-    //     //     let cell = row.insertCell();
-    //     //     let text = document.createTextNode(element[key]);
-    //     //     cell.appendChild(text);
-    //     // }
-    // }
 }
